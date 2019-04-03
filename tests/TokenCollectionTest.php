6af51ef3c6ede5a6155046d25593b9ca45b62ad2
Arne Blankerts <Arne@Blankerts.de>
2019-04-03 20:21:50 +0200

    Make Tests PHPUnit 8 compatible

--- tests/TokenCollectionTest.php
+++ tests/TokenCollectionTest.php
@@ -11,7 +11,7 @@ class TokenCollectionTest extends TestCase {
     /** @var  TokenCollection */
     private $collection;
 
-    protected function setUp() {
+    protected function setUp(): void {
         $this->collection = new TokenCollection();
     }
 
@@ -33,7 +33,7 @@ class TokenCollectionTest extends TestCase {
         $this->collection->addToken($token);
 
         foreach($this->collection as $position => $current) {
-            $this->assertInternalType('integer', $position);
+            $this->assertIsInt($position);
             $this->assertSame($token, $current);
         }
     }
